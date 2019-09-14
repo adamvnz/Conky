add75d2518e25688b1bcccca5bb28ac0eeacc899
Adam Vitali <adamvnz@techie.com>
2019-09-14 13:44:35 +1200

    Add files via upload

---
+++ Conky Rings Basic/imap.pl
@@ -0,0 +1,36 @@
+#!/usr/bin/perl
+ 
+# by gxmsgx
+# description: get the count of unread messages on imap
+
+use strict;
+use Mail::IMAPClient;
+use IO::Socket::SSL;
+
+my $username = 'put username here'; 
+my $password = 'put password here'; 
+
+my $socket = IO::Socket::SSL->new(
+  PeerAddr => 'imap.mail.com',
+  PeerPort => 993
+ )
+ or die "socket(): $@";
+ 
+my $client = Mail::IMAPClient->new(
+  Socket   => $socket,
+  User     => $username,
+  Password => $password,
+ )
+ or die "new(): $@";
+ 
+if ($client->IsAuthenticated()) {
+  my $msgct;
+
+  $client->select("INBOX");
+  $msgct = $client->unseen_count||'0';
+  print "${msgct}";
+  print " new"
+}
+
+$client->logout();
+
